218a8f0aba972ab297ec5d91c0855b438869d4ae
Hugo Carvalho <hugosc@gmail.com>
2020-04-02 21:57:10 -0300

    Add config server Zeit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000
 
 // ------  APP do EXPRESS -----
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // ------ Páginas ejs -------
 app.get('/', (request, response) => {
@@ -23,7 +26,7 @@ app.get('/faleconosco', (request, response) => {
     response.render('faleconosco')
 })
 // ------ SERVIDOR -----
-app.listen(3000,(err) => {
+app.listen(port,(err) => {
     if(err){
         console.log('Erro ao iniciar o servidor')
     }else{
